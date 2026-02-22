4a98cd2cb6d647165aa89e72d681d8384b611c75
Ademir Ferreira <whoisademir@gmail.com>
2026-02-21 21:08:53 -0300

    feat(auth): configure auth token generation

---
+++ src/AdobeMPClient/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using AdobeMPClient.Configuration;
+using AdobeMPClient.Implementations;
+using AdobeMPClient.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdobeMPClient.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAdobeClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<AdobeSettings>(configuration.GetSection(AdobeSettings.SectionName));
+        services.AddHttpClient<IAdobeClient, AdobeClient>("adobeClient");
+
+        return services;
+    }
+}
---
+++ src/AdobeMPClient/Implementations/AdobeClient.cs
@@ -0,0 +1,57 @@
+﻿using AdobeMPClient.Configuration;
+using AdobeMPClient.Interfaces;
+using Duende.IdentityModel.Client;
+using Microsoft.Extensions.Options;
+
+namespace AdobeMPClient.Implementations;
+
+public class AdobeClient(HttpClient httpClient, IOptions<AdobeSettings> options) : IAdobeClient
+{
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly AdobeSettings _adobeSettings = options.Value;
+
+    private readonly SemaphoreSlim _tokenSemaphore = new(1, 1);
+
+    private TokenResponse? _currentToken;
+    private DateTime _tokenExpiration;
+
+    private async Task<TokenResponse> GetAccessTokenAsync()
+    {
+        if (_currentToken != null && !string.IsNullOrEmpty(_currentToken.AccessToken) && DateTime.UtcNow < _tokenExpiration)
+        {
+            return _currentToken;
+        }
+
+        await _tokenSemaphore.WaitAsync().ConfigureAwait(false);
+
+        try
+        {
+            if (_currentToken != null && !string.IsNullOrEmpty(_currentToken.AccessToken) && DateTime.UtcNow < _tokenExpiration)
+            {
+                return _currentToken;
+            }
+
+            var tokenResponse = await _httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = $"{_adobeSettings.Ims}/ims/token/v3",
+                ClientId = _adobeSettings.ApiKey,
+                ClientSecret = _adobeSettings.ClientSecret,
+                Scope = "openid,AdobeID,read_organizations"
+            }).ConfigureAwait(false);
+
+            if (tokenResponse.IsError)
+            {
+                throw new Exception($"Falha na autenticação Adobe: {tokenResponse.Error} - {tokenResponse.ErrorDescription}", tokenResponse.Exception);
+            }
+
+            _currentToken = tokenResponse;
+            _tokenExpiration = DateTime.UtcNow.AddSeconds(tokenResponse.ExpiresIn);
+
+            return tokenResponse;
+        }
+        finally
+        {
+            _tokenSemaphore.Release();
+        }
+    }
+}
